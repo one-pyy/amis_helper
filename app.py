228dd8d06376658033d26f03598c7c30f139f962
pyy <pyy.whoami@foxmail.com>
2022-05-12 17:48:49 +0800

    add webui

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from fastapiHelper import startAmis,sql,start_ui
 from fastapi import Body, FastAPI, Path, Request, Response
 import uvicorn
 
-runGUI=False
 app=FastAPI()
 startAmis(app,"/amis/set")
+runGUI=False
 
 if __name__ == '__main__':
-  if runGUI: 
+  if runGUI:
     start_ui(app)
   else:
     uvicorn.run("app:app",host="0.0.0.0",port=8080,debug=True,reload=True)
